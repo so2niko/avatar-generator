d2d8423a6c11c45a7dc9d73876db9fe68581c576
Nick Sotula <so2niko@gmail.com>
2020-05-11 11:38:57 +0300

    refactor: add standalone function of full random generating

--- app/avatar.js
+++ app/avatar.js
@@ -2,7 +2,7 @@ export default class Avatar{
     static state = null;
     size = 256;
     border = 8;
-    boxSize = 50;
+    boxSize = 48;
     bgColor = '#f5f5f5';
     color = '#333';
     canvasSelector = '.canvas-avatar';
@@ -17,16 +17,30 @@ export default class Avatar{
         this.ctx.clearRect(0, 0, this.size, this.size);
         this.ctx.fillStyle = this.bgColor;
         this.ctx.fillRect(0, 0, this.size, this.size);
-        for(let i = this.border; i < this.size; i += this.boxSize){
-            for(let j = this.border; j < this.size; j += this.boxSize){
+
+        this.renderRandomImage();
+    }
+
+    renderRandomImage = _ =>{
+        const paddingBorder = this.size - this.boxSize;
+        for(let i = this.border; i < paddingBorder; i += this.boxSize){
+            for(let j = this.border; j < paddingBorder; j += this.boxSize){
                 this.renderBox(i, j);
             }
         }
     }
 
-    renderBox(x, y){
-        this.ctx.fillStyle = Math.random() > .5? this.color: this.bgColor;
+    renderBox(x, y, color = 'random'){
+        if(color == 'random'){
+            color = Math.random() > .5? this.color: this.bgColor;
+        }
+        this.ctx.fillStyle = color;
         this.ctx.fillRect(x, y, this.boxSize, this.boxSize);
+    }
+
+    renderBaseLine = _ =>{
+        const xStart = this.border + this.boxSize * 2;
+        
     }
 
     static init(){
